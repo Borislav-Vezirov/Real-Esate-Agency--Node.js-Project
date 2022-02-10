0ba3c60dd062f8e65e71c9fad6d69b780701df13
Borislav Vezirov <bobi.vez@gmail.com>
2022-02-10 10:58:12 +0200

    made the configuration for handlebars

---
+++ src/config/hbsConfig.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const hbs  = require('express-handlebars');
+
+function hbsConfig(app){
+
+    app.engine('hbs', hbs.engine({
+        extname: 'hbs'
+    }));
+
+    app.set('view engine', 'hbs');
+    app.set('views', './src/views');
+}
+
+module.exports = hbsConfig;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const expressConfig = require('./config/expressConfig.js');
+const hbsConfig = require('./config/hbsConfig.js');
 
 const app = express();
 const port = 3000;
 
+hbsConfig(app);
+expressConfig(app);
+
 app.get('/', (req, res) => {
-    res.send('The App is running')
+    res.render('home');
 });
 
 app.listen(port, () => console.log(`The app is running on http://localhost:${port}`));
