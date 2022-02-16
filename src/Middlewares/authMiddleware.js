5e9d024a3128942d414cf0ab693d6388716ea82e
Borislav Vezirov <bobi.vez@gmail.com>
2022-02-16 07:35:45 +0200

    add guard for isOwner

--- src/Middlewares/authMiddleware.js
+++ src/Middlewares/authMiddleware.js
@@ -1,4 +1,5 @@
 const { AUTH_COOKIE_NAME, JWT_SECRET } = require('../config/index.js');
+const { getOne } = require('../services/housingServices.js');
 const jwt = require('../utils/jwt.js');
 
 
@@ -37,8 +38,29 @@ function isAuth(req, res, next) {
     }
 };
 
+function isGuests(req, res, next){
+
+    if(!req.user){
+        next();
+    }else{
+        res.redirect('/')
+    }
+};
+
+async function isOwner(req, res, next, id){
+
+    const house = await getOne(id);
+
+    if(house._id == req.user.id){
+        res.redirect('/');
+    }
+    next();
+}
+
 
 module.exports = {
     auth,
-    isAuth
+    isAuth,
+    isGuests,
+    isOwner
 }
