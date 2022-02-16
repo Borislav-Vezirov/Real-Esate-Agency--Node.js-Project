c046c48f4fa7dd82e6bf5f6e1f192d03f830143f
Borislav Vezirov <bobi.vez@gmail.com>
2022-02-16 09:26:04 +0200

    add additional validators to the schema

--- src/models/Housing.js
+++ src/models/Housing.js
@@ -2,20 +2,20 @@ const mongoose = require('mongoose');
 
 
 const userSchema = new mongoose.Schema({
-        name            : {type: String, required: true},
-        type            : {type: String, enum: ['Apratment','Villa', 'House'], required: true},
-        year            : {type: Number, required: true},
-        city            : {type: String, required: true},
-        image           : {type: String, required: true},
-        description     : {type: String, required: true},
-        availablePieces : {type: Number, required: true},
-        tenants         : [{type: mongoose.Types.ObjectId, ref: 'User'}],
-        owner           : {type: mongoose.Types.ObjectId, ref: 'User'}
+    name            : {type: String, required: true, minlength: 6},
+    type            : {type: String, enum: ['Apratment','Villa', 'House'], required: true},
+    year            : {type: Number, required: true, min: 1850, max: 2021},
+    city            : {type: String, required: true, minlength: 4},
+    image           : {type: String, required: true, match: /^https?:\/\//i},
+    description     : {type: String, required: true, maxlength: 60},
+    availablePieces : {type: Number, required: true, min: 0, max: 10},
+    tenants         : [{type: mongoose.Types.ObjectId, ref: 'User'}],
+    owner           : {type: mongoose.Types.ObjectId, ref: 'User'}
 
-    },
-    {
-        timestamps: true
-    }
+},
+{
+    timestamps: true
+}
 );
 
 userSchema.method('getTenants', function(){
